9f162771b4397aa5e7df976968599a5e0907c35c
Tsabit Gholib <tsabit1502@gmail.com>
2025-02-05 11:48:25 +0700

    update qris fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -39,7 +39,7 @@ class AuthController extends Controller
                 'jenis_kelamin' => $validatedData['jenis_kelamin'],
                 'sekolah' => $validatedData['sekolah'],
                 'password' => bcrypt($validatedData['password']),
-                'tagihan' => '1',
+                'tagihan' => '9000',
                 'created_time' => substr((string) intval(microtime(true) * 1000), -8),
                 'va_number' => (int) $vaNumber,
                 'lunas' => false
